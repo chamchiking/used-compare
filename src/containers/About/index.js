1be9016c62d3de933b3e45fe1872a378948cc436
nhkim <lmcr136a@gmail.com>
2022-06-20 11:51:04 +0900

    디자인 수정

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -5,22 +5,23 @@ export default function About() {
   return (
     <>
     <Container>
-      <div id='about-first' class="d-flex align-items-start mt-5 mb-3">
-        <div class="d-grid gap-3">
-            <div  class="p-3 mb-2 bg-secondary text-white">
-                <p className="fs-1">Save Your Money and Time!</p>
+      <div class="my-5 py-5 fs-1">New & SecondHands</div>
+      <div id='about-first' class="d-flex align-items-start mt-5 mb-3 mx-5">
+        <div class="d-grid gap-3 text-start" >
+            <div  class="p-3 mb-2 bg-secondary text-white text-center">
+                <p className="fs-2">Save Your Money and Time!</p>
             </div>
 
             <div  class="p-3 mb-2 bg-light text-dark">
-            <p class="fs-3">Sick of hopping around here and there? Find everything you need here at once.</p>
+            <p class="fs-4">Sick of hopping around here and there? Find everything you need here at once. Our service provides convenience that we have never experienced before.</p>
             </div>
 
             <div  class="p-3 mb-2 bg-light text-dark">
-                <p className="fs-3">Don't compromise your time and money! They do matter. </p>
+                <p className="fs-4">Don't compromise your time and money! They do matter. </p>
             </div>
 
             <div className="p-3 mb-2 bg-light text-dark">
-                <p className="fs-3">Just start here with us. We've got your back. </p>
+                <p className="fs-4">Just start here with us. We've got your back. You can find the best product just by searching once.</p>
             </div>
 
 
@@ -31,25 +32,25 @@ export default function About() {
     </Container>
 
     <Container>
-      <div id='about-second' class="d-flex align-items-start mt-5 mb-3" >
+      <div id='about-second' class="d-flex align-items-start mt-5 mb-3  mx-5" >
         <img id='about-second-img' src={require('./earth.jpg')} width="500" height="500" />
 
-        <div class="d-grid gap-3">
-             <div class="p-3 mb-2 bg-secondary text-white">
-                 <p className="fs-1">Save The Mother Earth!</p>
+        <div class="d-grid gap-3 text-start">
+             <div class="p-3 mb-2 bg-secondary text-white text-center">
+                 <p className="fs-2">Save The Mother Earth!</p>
              </div>
 
              <div class="p-3 mb-2 bg-light text-dark">
-                 <p className="fs-3">Why buy new goods when you can purchase used ones at better price?
+                 <p className="fs-4">Why buy new goods when you can purchase used ones at better price?
                  </p>
              </div>
 
              <div class="p-3 mb-2 bg-light text-dark">
-                 <p className="fs-3">Don't know where to start? Here we are to help you choose better.</p>
+                 <p className="fs-4">Don't know where to start? Here we are to help you choose better.</p>
              </div>
 
              <div class="p-3 mb-2 bg-light text-dark">
-                 <p className="fs-3">The more you shop smart, the less we harm the planet. </p>
+                 <p className="fs-4">The more you shop smart, the less we harm the planet. Don't miss a choice that's only good points. </p>
              </div>
 
 
